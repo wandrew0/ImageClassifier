7d7f66ec3afffaac9ffde203c1c517d4b651df90
wandrew0 <awang22@terpmail.umd.edu>
2024-07-18 15:34:12 -0700

    ?

--- src/pages/NotLoggedInError.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ErrorBoxWithLink from "./ErrorBoxWithLink.js";
-const NotLoggedInError = () => {
-    return (
-    <ErrorBoxWithLink errorMessage="You are not logged in!" link="/login" linkText="Sign In" />
-    )
-}
-export default NotLoggedInError;
