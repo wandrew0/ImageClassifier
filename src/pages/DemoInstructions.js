23fa39dab9583d6944cc9e5949ce44836c3933c5
wandrew0 <awang22@terpmail.umd.edu>
2024-08-05 19:25:38 -0700

    demoninstructions

--- src/pages/DemoInstructions.js
+++ src/pages/DemoInstructions.js
@@ -1,9 +1,37 @@
 const DemoInstructions = () => {
     return (
-        <div className='demo-instructions'>
-
+        <div className="demo-instructions">
+            <h3>
+                Click any of the links on the top, or read on for a little
+                description.
+            </h3>
+            <p>
+                (Local) versions download and run the models directly in your
+                browser, while (Server) versions call the Node backend.
+            </p>
+            <p>
+                <b>Warning:</b> clicking Doodle (local) will download a 90 MB
+                file every time. The Character model however is only 1.5 MB.
+            </p>
+            <p>
+                Doodle was trained on the Quick, Draw! dataset, specifically the
+                preprocessed, 28x28 grayscale and centered Numpy bitmaps, and
+                the model I use is PyTorch's implementation of EfficientNetV2 S.
+            </p>
+            <p>
+                Characters were trained on the EMNIST balanced dataset, which
+                means some letters only have uppercase versions (if the
+                lowercase looks similar), and I use a simple convolutional
+                neural network.
+            </p>
+            <p>
+                On the left of each page are the available classes. In the
+                center is the drawing canvas, and on the right is the downscaled
+                and centered input to the model (black and white are also
+                flipped).
+            </p>
         </div>
     );
-}
+};
 
 export default DemoInstructions;
--- src/pages/RootWin.js
+++ src/pages/RootWin.js
@@ -1,13 +1,11 @@
-import '../css/styles.css';
+import "../css/styles.css";
 import DemoInstructions from "./DemoInstructions";
 const RootWin = () => {
     return (
-    <div>
-        <h1 className="blueHeader">
-            Welcome to Image Classifier Demo </h1>
-        <DemoInstructions/>
-
-    </div>
-    )
-}
+        <div>
+            <h1 className="blueHeader">Welcome to Image Classifier Demo </h1>
+            <DemoInstructions />
+        </div>
+    );
+};
 export default RootWin;
