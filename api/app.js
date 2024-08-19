3420c97bc9f319d4e784837684e194c076bcef7e
wandrew0 <awang22@terpmail.umd.edu>
2024-08-18 19:41:11 -0700

    temp update log middleware

--- api/app.js
+++ api/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const logger = require("./logger");
 
 const quick_draw_router = require("./routes/quick_draw_routes");
 const emnistb_router = require("./routes/emnistb_routes");
@@ -9,6 +10,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use((req, res, next) => {
+    logger.http(req.ip);
+    next();
+});
 
 app.use(`/api/quickdraw`, quick_draw_router);
 app.use(`/api/emnistb`, emnistb_router);
