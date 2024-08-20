44780e0ea0848de1ad3f2f520400780edf805cbb
wandrew0 <awang22@terpmail.umd.edu>
2024-08-19 18:06:33 -0700

    update logging

--- api/app.js
+++ api/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const logger = require("./logger");
+const Resolver = require("dns/promises").Resolver;
 
 const quick_draw_router = require("./routes/quick_draw_routes");
 const emnistb_router = require("./routes/emnistb_routes");
 const recaptcha_router = require("./routes/recaptcha");
+
+const resolver = new Resolver();
 
 const app = express();
 
@@ -12,7 +15,20 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-    logger.http(req.ip);
+    const split = req.ip.split(":");
+    const ip = split[split.length - 1];
+
+    resolver
+        .reverse(ip)
+        .then((host) => {
+            logger.http(`${req.originalUrl} - ${ip} - ${host}`);
+        })
+        .catch((err) => {
+            logger.http(`${req.originalUrl} - ${ip}`);
+            logger.error(err.code === "ECANCELLED" ? "reverse dns timeout" : err);
+        });
+    setTimeout(() => resolver.cancel(), 5000);
+
     next();
 });
 
@@ -21,3 +37,14 @@ app.use(`/api/emnistb`, emnistb_router);
 app.use(`/api/recaptcha`, recaptcha_router);
 
 module.exports = app;
+
+// resolver
+//     .reverse(ip)
+//     .then((host) => {
+//         console.log(`${ip} - ${host}`);
+//     })
+//     .catch((err) => {
+//         console.log(`${ip}`);
+//         console.log(err.code === "ECANCELLED" ? "reverse dns timeout" : err);
+//     });
+// setTimeout(() => resolver.cancel(), 0);
